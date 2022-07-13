c2eaf54f5cdabda26bf1a85f3991e326571bacb2
camila-romero-cyb <camilaromero085@gmail.com>
2022-07-12 21:41:14 -0300

    modo oscuro

--- archivo.js
+++ archivo.js
@@ -1,1 +1,9 @@
-alert("hola")
+const btnSwitch = document.getElementById("darkmode");
+const body = document.body;
+
+darkmode.addEventListener("click", function () {
+    body.classList.toggle("dark");
+    
+})
+
+
